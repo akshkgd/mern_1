65ec2c6dd1b046b68380920895121899ea02c6bd
Ashish Shukla <akshkgd@gmail.com>
2021-09-24 21:07:20 +0530

    js day

---
+++ js day 1/js/add.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(prompt('enter first number')); 
+let num2 = parseInt(prompt('enter second number')); 
+alert('the sum of the number is ' + num1 + num2);
+
+
+console.log(typeof(num1))
---
+++ js day 1/js/script.js
@@ -0,0 +1,71 @@
+let name = 'prabhakar';
+var age = 24;
+const gender = 'Male'; 
+name = 'Prabhakar Bhagwat'
+age = 30;
+
+
+
+console.log(name);
+console.log(age);
+console.log(gender);
+let label;
+console.log(typeof(label))
+
+// js datatypes
+
+let name = 'a';
+let age = 24.6;
+let isAlive = true;
+let hobby = ['book reading', 'coding', 'Music', 'Swiming', true, 23];
+let user = {
+	name: 'ashish',
+	email: 'ashish@codekaro.in',
+	mobile: 1234567890,
+	address: 'New Delhi, India',
+	intrests: ['Javascript', 'react js', 'Node js', 'laravel', 12]
+}
+
+let users = [
+	{
+	name: 'ashish',
+	email: 'ashish@codekaro.in',
+	mobile: 1234567890,
+	address: 'New Delhi, India',
+	intrests: ['Javascript', 'react js', 'Node js', 'laravel'],
+		label: 'admin',
+	},
+	{
+		name: 'Adithya',
+	email: 'ashish@codekaro.in',
+	mobile: 1234567890,
+	address: 'New Delhi, India',
+	intrests: ['Javascript', 'react js', 'Node js', 'laravel', 12]
+	},
+	{
+		name: 'Tanzeela',
+	email: 'ashish@codekaro.in',
+	mobile: 1234567890,
+	address: 'New Delhi, India',
+	intrests: ['Javascript', 'react js', 'Node js', 'laravel', 12]
+	},
+	{
+	name: 'Neema',
+	email: 'ashish@codekaro.in',
+	mobile: 1234567890,
+	address: 'New Delhi, India',
+	intrests: ['Javascript', 'react js', 'Node js', 'laravel', 12]
+	}
+]
+
+console.log(users[2].name + " " + users[1].name);
+
+
+
+console.log(user);
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isAlive));
+console.log(typeof(user));
+console.log(hobby);
